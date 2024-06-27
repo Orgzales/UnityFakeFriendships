2a4c82c49e2d9b4b2371f62b86df778c2d9baf5b
Orgzales <77345323+Orgzales@users.noreply.github.com>
2024-06-27 19:11:18 -0400

    Update PlayerRunAwayTest.cs

--- FAKEFRIENDSHIPS/Assets/Scripts/PlayerRunAwayTest.cs
+++ FAKEFRIENDSHIPS/Assets/Scripts/PlayerRunAwayTest.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerRunAwayTest : MonoBehaviour
 {
+
+    public GameObject player;
+    public GameObject closest_Exit;
+
+    public float playerSpeed = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //find the nearest exit and make it the cloest_Exit
+        GameObject[] exits = GameObject.FindGameObjectsWithTag("Exit");
+        float minDistance = Mathf.Infinity;
+        foreach (GameObject exit in exits)
+        {
+            float distance = Vector3.Distance(player.transform.position, exit.transform.position);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                closest_Exit = exit;
+            }
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //have players move to the cloest exit
+        player.transform.position = Vector3.MoveTowards(player.transform.position, closest_Exit.transform.position, playerSpeed * Time.deltaTime);
+
     }
 }
